b0bf2c5f75a9a988a4a00339fac0a6f633cbf9cf
Tom John Hall <thomasjohnhall@gmail.com>
2017-06-09 18:36:04 +0100

    added allowed hosts

--- lobbyist/settings.py
+++ lobbyist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://salty-hamlet-46189.herokuapp.com/', 'https://salty-hamlet-46189.herokuapp.com/', 'http://lobbyist.org.uk']
+ALLOWED_HOSTS = ['127.0.0.1', 'herokuapp.com', 'salty-hamlet-46189.herokuapp.com/', 'https://salty-hamlet-46189.herokuapp.com/', 'lobbyist.org.uk']
 
 
 # Application definition
