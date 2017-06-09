4a1065a24cd8d5b0dbb49f08d401b413c61a0ebb
Tom John Hall <thomasjohnhall@gmail.com>
2017-06-09 17:44:12 +0100

    ran collectstatic

--- lobbyist/settings.py
+++ lobbyist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'mwt7mea14j=t&ux%me9e$a=1*$iiwdjosecc#g3eq6sp3mzpo+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
