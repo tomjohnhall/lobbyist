6bd441390a19ddff72c0dc9504dc44e41ed9ce66
Tom John Hall <thomasjohnhall@gmail.com>
2017-07-24 17:22:01 +0100

    First full draft, set for deploy

--- lobbyist/settings.py
+++ lobbyist/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mwt7mea14j=t&ux%me9e$a=1*$iiwdjosecc#g3eq6sp3mzpo+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost', 'herokuapp.com', 'salty-hamlet-46189.herokuapp.com', 'lobbyist.org.uk']
 
