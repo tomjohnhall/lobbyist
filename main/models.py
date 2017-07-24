a2be6fd67106a2ce177c40ed6fcfd2146e74a655
Tom John Hall <thomasjohnhall@gmail.com>
2017-07-24 20:37:07 +0100

    charfield lengths

--- main/models.py
+++ main/models.py
@@ -6,18 +6,18 @@ from django.db import models
 
 class Event(models.Model):
     date = models.DateField(blank=True, null=True)
-    house = models.CharField(max_length=999)
-    location = models.CharField(max_length=999)
-    title = models.CharField(max_length = 999)
-    description = models.CharField(max_length=999)
+    house = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     def __unicode__(self):
         return unicode(self.title)
 
 class Consultation(models.Model):
     gov_id = models.CharField(max_length=20)
     date = models.DateTimeField(blank=True, null=True)
-    title = models.CharField(max_length = 500)
-    summary = models.CharField(max_length = 500)
-    url = models.CharField(max_length = 500)
+    title = models.CharField(max_length = 255)
+    summary = models.CharField(max_length = 255)
+    url = models.CharField(max_length = 255)
     def __unicode__(self):
         return self.title
