550d1330f82947d46954db4d50de701f6c075c79
TJH <thomasjohnhall@gmail.com>
2017-12-27 13:09:09 +0000

    altered function for only past events available to show most recent

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -12,8 +12,8 @@ function expander(arrow,content) {
 $(document).ready(function() {
 
   if($('.future').length == 0) {
-    $('.date:nth-of-type(-n+3)').addClass('importance');
-    $('#recent-events').hide();
+    $('.date:nth-of-type(n+3)').addClass('importance');
+    $('#more-events').hide();
   }
 
   $('#mp-title').click(function () {
